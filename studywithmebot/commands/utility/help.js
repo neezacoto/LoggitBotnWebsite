52adf521e8af728bce8945b7fbdb2c1da576a89d
neezacoto <ringthebellpepper@gmail.com>
2021-04-18 15:12:28 -0400

    help command works now

--- studywithmebot/commands/utility/help.js
+++ studywithmebot/commands/utility/help.js
@@ -10,6 +10,7 @@ module.exports = {
     cooldown: 10,
     execute(message,args)
     {
+        const client = message.client;
         // getting the commands collection from the messages client coming from the index
         const { commands } = message.client;
         const fun = [];
@@ -18,27 +19,21 @@ module.exports = {
 
         const commandFolders = fs.readdirSync('./commands');
 
-        for(const command of commands)
-        {
-
-        }
-
         if (!args.length) {
             const embed = new Discord.MessageEmbed()
-                .setTitle('Here\'s a list of all my commands:')
+                .setTitle(`Here\'s a list of all my commands:`)
                 .setThumbnail(message.client.user.avatarURL())
                 .setColor("#127afa")
                 .setFooter(`Current prefix is "${prefix}"`,client.user.avatarURL())
                 .addFields(
                     {
                         name: "Commands",
-                        value: commands.map(
+                        value: (commands.map(
                             command => `**${prefix}${command.name}:** ${command.description || 'no description'}`
-                        ).join('\n')
-                    }
-                );
+                        ).join('\n')).toString()
+                    });
 
-            message.channel.send(embed,{ split: true});
+            message.reply(embed);
             //dms method if I even want to do that
             // return message.author.send(embed, { split: true })
             //     .then(() => {
@@ -50,24 +45,24 @@ module.exports = {
             //         message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
             //     });
         }
-
-        const name = args[0].toLowerCase();
-        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
-        if (!command) {
-            return message.reply('that\'s not a valid command!');
+        else if(args.length) {
+            try {
+                const name = args[0].toLowerCase();
+                const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+                const toSay = (`\n**${prefix}${command.name}:**
+Description: ${command.description || 'no description'}
+Usage: \`\`${prefix}${command.name} ${command.usage || `no usage description`}\`\``);
+                message.reply(toSay);
+            }
+            catch(error)
+            {
+                message.reply(`${args[0]} is not a command.`)
+            }
+        }
+        else{
+            message.reply(`\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``)
         }
 
-        data.push(`**Name:** ${command.name}`);
-
-        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-        if (command.description) data.push(`**Description:** ${command.description}`);
-        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-        message.channel.send(data, { split: true });
-
-    },
+    }
 
 }
